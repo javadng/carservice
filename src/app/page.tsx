bd1d5940e788ee85b73d15f3f55d2126131aaecf
javadng <javadnasirifu@gmail.com>
2023-07-02 14:13:48 +0330

    master: slider done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,26 @@
-import Image from "next/image";
+import Slider from "@/components/slider";
+
+const images = [
+  {
+    id: "im1",
+    url: "/image/carousel-bg-1.jpg",
+    subtitle: "CAR SERVICING",
+    title: "Qualified Car Wash Service Center",
+    btnText: "learn more!",
+  },
+  {
+    id: "im2",
+    url: "/image/carousel-bg-2.jpg",
+    subtitle: "Fix cars",
+    title: "Expert Car Services for Reliable Maintenance and Repairs",
+    btnText: "learn about us!",
+  },
+];
 
 export default function Home() {
-  return <h1>home</h1>;
+  return (
+    <section>
+      <Slider images={images} />
+    </section>
+  );
 }
---
+++ src/components/slider.tsx
@@ -0,0 +1,107 @@
+"use client";
+
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import Image from "next/image";
+import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
+
+interface ImageObj {
+  id: string;
+  url: string;
+  subtitle: string;
+  title: string;
+  btnText: string;
+}
+
+interface ImageSliderProps {
+  images: ImageObj[];
+}
+
+const Slider: React.FC<ImageSliderProps> = ({ images }) => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const nextImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+  };
+
+  const prevImage = () => {
+    setCurrentImageIndex((prevIndex) => {
+      if (prevIndex === 0) {
+        return images.length - 1;
+      } else {
+        return prevIndex - 1;
+      }
+    });
+  };
+
+  const containerVariants = {
+    hidden: (custom: number) => ({
+      opacity: 0,
+      x: custom > 0 ? 100 : -100,
+    }),
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        type: "tween",
+        duration: 0.5,
+      },
+    },
+  };
+
+  return (
+    <div className="slider relative overflow-hidden whitespace-nowrap">
+      <motion.div
+        className="image-container"
+        key={currentImageIndex}
+        custom={currentImageIndex}
+        variants={containerVariants}
+        initial="hidden"
+        animate="visible"
+      >
+        <figure className="relative ">
+          <Image
+            width={1000}
+            height={1000}
+            className="w-full"
+            quality={100}
+            src={images[currentImageIndex].url}
+            alt={`Image ${currentImageIndex}`}
+          />
+          <div className="overlay absolute left-0 top-0 w-full h-full bg-blend-multiply bg-black bg-opacity-60"></div>
+        </figure>
+        <motion.div
+          initial={{ translateY: "-10%", scale: 0.5 }}
+          animate={{ translateY: 0, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className="text-white absolute top-[10%] w-[80%] left-[10%] text-center"
+        >
+          <h4 className="my-5 uppercase">
+            {images[currentImageIndex].subtitle}
+          </h4>
+          <h1 className="text-5xl mb-14 w-2/3 whitespace-normal mx-auto font-bold">
+            {images[currentImageIndex].title}
+          </h1>
+          <button className="bg-primary py-3 px-14 rounded-lg text-xl uppercase">
+            {images[currentImageIndex].btnText}
+          </button>
+        </motion.div>
+      </motion.div>
+
+      <div
+        className="control-left absolute left-0 top-0 h-full text-white text-5xl mx-5 flex items-center cursor-pointer"
+        onClick={prevImage}
+      >
+        <AiOutlineLeft className="hover:scale-150 transition duration-300" />
+      </div>
+      <div
+        className="control-right absolute right-0 top-0 h-full text-white text-5xl mx-5 flex items-center cursor-pointer"
+        onClick={nextImage}
+      >
+        <AiOutlineRight className="hover:scale-150 transition duration-300" />
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
