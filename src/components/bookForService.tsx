b970fa815d85c0c72e78fe058bb0420efc76e5ab
javadng <javadnasirifu@gmail.com>
2023-07-05 17:15:11 +0330

    master: footer done

--- src/components/bookForService.tsx
+++ src/components/bookForService.tsx
@@ -4,24 +4,26 @@ import ScrollPullAnimated from "./scrollPullAnimated";
 
 const BookForService = () => {
   return (
-    <section className="book-service my-12 text-white px-10 gap-10 grid md:grid-cols-2">
-      <ScrollPullAnimated offscreenY={300} onscreenY={0}>
-        <div className="desc p-8">
-          <h1 className="capitalize text-2xl md:text-4xl font-bold my-6">
-            Certified and Award Winning Car Repair Service Provider
-          </h1>
-          <p>
-            Eirmod sed tempor lorem ut dolores. Aliquyam sit sadipscing kasd
-            ipsum. Dolor ea et dolore et at sea ea at dolor, justo ipsum duo
-            rebum sea invidunt voluptua. Eos vero eos vero ea et dolore eirmod
-            et. Dolores diam duo invidunt lorem. Elitr ut dolores magna sit. Sea
-            dolore sanctus sed et. Takimata takimata sanctus sed.
-          </p>
-        </div>
-      </ScrollPullAnimated>
-      <ScrollPullAnimated offscreenY={300} onscreenY={0} scale={true}>
-        <BookForm />
-      </ScrollPullAnimated>
+    <section className="book-service my-12 text-white px-10">
+      <div className="gap-10 grid md:grid-cols-2 container w-5/6 mx-auto">
+        <ScrollPullAnimated offscreenY={300} onscreenY={0}>
+          <div className="desc p-8">
+            <h1 className="capitalize text-2xl md:text-4xl font-bold my-6">
+              Certified and Award Winning Car Repair Service Provider
+            </h1>
+            <p>
+              Eirmod sed tempor lorem ut dolores. Aliquyam sit sadipscing kasd
+              ipsum. Dolor ea et dolore et at sea ea at dolor, justo ipsum duo
+              rebum sea invidunt voluptua. Eos vero eos vero ea et dolore eirmod
+              et. Dolores diam duo invidunt lorem. Elitr ut dolores magna sit.
+              Sea dolore sanctus sed et. Takimata takimata sanctus sed.
+            </p>
+          </div>
+        </ScrollPullAnimated>
+        <ScrollPullAnimated offscreenY={300} onscreenY={0} scale={true}>
+          <BookForm />
+        </ScrollPullAnimated>
+      </div>
     </section>
   );
 };
